cf9326c0782a834ba9e5ddb9697e0263340ee5e3
aubin-pro <127711433+aubin-pro@users.noreply.github.com>
2023-04-11 22:22:51 +0200

    Create zachApp.groovy

---
+++ vars/zachApp.groovy
@@ -0,0 +1,31 @@
+def call(String repoUrl){
+pipeline{
+	agent any
+	stages{
+		stage('1-clone'){
+			steps{
+				checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'team5-id', url: 'https://github.com/Team5-group6/parralele-job.git']])
+			}
+		}
+		stage('2-parallel-stage'){
+			parallel{
+				stage('1-parallel-saada-job'){
+					steps{
+						sh'lscpu'
+					}
+				}
+				stage('2-parallel-aubin-job'){
+					steps{
+						sh 'du -h'
+					}
+				}
+			}
+		}
+		stage('demo-app3'){
+			steps{
+				echo 'shared-library was successful'
+			}
+		}
+	}
+}	
+}
