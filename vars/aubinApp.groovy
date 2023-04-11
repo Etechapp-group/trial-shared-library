328389b3afb888a85955f61f22a6c9723c1a8fbe
aubin-pro <127711433+aubin-pro@users.noreply.github.com>
2023-04-11 22:02:06 +0200

    Update aubinApp.groovy

--- vars/aubinApp.groovy
+++ vars/aubinApp.groovy
@@ -1,1 +1,26 @@
-
+def call(String repoUrl){
+pipeline{
+	agent any
+	stages{
+		stage('1-clone'){
+			steps{
+				checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'team5-id', url: 'https://github.com/Team5-group6/parralele-job.git']])
+			}
+		}
+		stage('2-parallel-stage'){
+			parallel{
+				stage('1-parallel-saada-job'){
+					steps{
+						sh'lscpu'
+					}
+				}
+				stage('2-parallel-aubin-job'){
+					steps{
+						sh 'du -h'
+					}
+				}
+			}
+		}
+	}
+}	
+}
